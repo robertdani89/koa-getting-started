8f3173b67f50d3b7deb510cf21d0c45c080638f5
robert-dani <robert.dani@onrobot.com>
2022-01-26 14:52:44 +0100

    added first test

--- 1_hello/app.js
+++ 1_hello/app.js
@@ -1,5 +1,5 @@
 const Koa = require('koa');
-const app = new Koa();
+const app = module.exports = new Koa();
 
 // logger
 app.use(async (ctx, next) => {
@@ -27,4 +27,4 @@ app.use(async ctx => {
     ctx.body = 'Hello World';
 });
 
-app.listen(3000);
+if (!module.parent) app.listen(3000);
